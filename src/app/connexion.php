e56e9d01a10a3d7e503a234c7a07edf16d6e7a15
Noemie Lecherf <noemie.lecherf@fr.enersys.com>
2021-01-13 09:19:00 +0100

    resolution conflit

--- src/app/connexion.php
+++ src/app/connexion.php
@@ -3,7 +3,7 @@
 function connect() {
     try {
         //$db = new PDO('mysql:host=' . $config['serveur'] . ';dbname=' . $config['bd'], $config['login'], $config['mdp']);
-        $db = new PDO("mysql:host=localhost;dbname=gestionstock","login4064","KkCQvRxWbbnlgFz");
+        $db = new PDO("mysql:host=localhost;dbname=gestionstock2","root","");
     } 
     catch (Exception $e) {
         $db = NULL;
